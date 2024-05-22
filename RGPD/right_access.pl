4ffa9e51693383890476f3ca67705219c1b5d40b
poline <pauline.disalvocilia@gmail.com>
2024-05-22 08:27:28 +0200

    correction de la détection de l'envoi de données (mise en relation de la demande avec la réponse associée)

--- RGPD/right_access.pl
+++ RGPD/right_access.pl
@@ -3,12 +3,13 @@ writeRightAccessNotCompliant(S,TE):- write('right access not compliant : subject
 writeNoAccessAsked():- writeln('system compliant on right access as no asked was asked').
 
 accessTimeLimitNotOver(_P,T):- (tCurrent(TCURRENT),tLimit('access',TLIMITACCESS),(TCURRENT-T)<TLIMITACCESS).
-rightAccessOk(P,TE):-
+
+rightAccessOk(P,A,TE):-
 	(accessTimeLimitNotOver(P,TE), !);
-	(wasControlledBy(P2,_S2,'owner',_TB2,TE2),action(P2,'sendData'),(tLimit('access',TLIMITACCESS),(TE2-TE)<TLIMITACCESS), !).
+	(wasControlledBy(P2,_S2,'owner',_TB2,TE2),used(P2,A,_R,_T),action(P2,'sendData'),(tLimit('access',TLIMITACCESS),(TE2-TE)<TLIMITACCESS), !).
 
 rightAccess(S):-
-	wasControlledBy(P,S,'owner',_TB,TE),action(P,'askDataAccess'),
-		(rightAccessOk(P,TE);
-		(\+ rightAccessOk(P,TE), writeRightAccessNotCompliant(S,TE)));
-	(\+ (wasControlledBy(P,S,'owner',_TB,_TE),action(P,'askDataAccess')), writeNoAccessAsked()).
+    wasControlledBy(P,S,'owner',_TB,TE), wasGeneratedBy(A,P,_R,_T), action(P,'askDataAccess'),
+    (rightAccessOk(P,A,TE);
+    		(\+ rightAccessOk(P,A,TE), writeRightAccessNotCompliant(S,TE)));
+    	(\+ (wasControlledBy(P,S,'owner',_TB,_TE),action(P,'askDataAccess')), writeNoAccessAsked()).
