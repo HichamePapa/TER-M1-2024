e65d57c6bedbc69ae01e71ad178110a9b315fb13
ghiles14 <148078047+ghiles14@users.noreply.github.com>
2024-05-23 19:27:27 +0200

    Update testConverter.java

--- Traducteur/testConverter.java
+++ Traducteur/testConverter.java
@@ -13,9 +13,9 @@ public class testConverter {
     @Test
     public void testNullQuery(){
         list.add("Lawfullness");
-        c = new Converter( list, "chemin", null , null);
+        c = new Converter( list, "chemin", null , null, null);
         listQuerries = c.ConvertToPrologQuery();
-        String queryExpected = "?− legal(P, D, C, T G, T).";
+        String queryExpected = "?− legal(P, D, C, TG, T).";
         assertEquals(queryExpected, listQuerries.get(0));
     }
 
@@ -24,10 +24,10 @@ public class testConverter {
     public void testQueryWithValues(){
         list.add("Lawfullness");
         list.add("Right-to-access");
-        c = new Converter( list, "chemin", "dateDeNaissance" , "Alex");
+        c = new Converter( list, "chemin", "dateDeNaissance" , "Alex", "consent");
         listQuerries = c.ConvertToPrologQuery();
 
-        String queryExpected1 = "?− legal(P, dateDeNaissance, C, T G, T).";
+        String queryExpected1 = "?− legal(consent, dateDeNaissance, C, TG, T).";
         String queryExpected2 = "?− rigthAccess(Alex).";
 
         assertEquals(queryExpected1, listQuerries.get(0));
@@ -39,7 +39,7 @@ public class testConverter {
         list.add("Lawfullness");
         list.add("Right-to-access");
         list.add("Right-to-erasure");
-        c = new Converter(list,"path", "prenom", "bob");
+        c = new Converter(list,"path", "prenom", "bob", null);
         listQuerries = c.ConvertToPrologQuery();
 
         int numberExpected = 3;
@@ -48,7 +48,7 @@ public class testConverter {
 
     @Test
     public void testEmptyQuery(){
-        c = new Converter(list,"path", "prenom", "bob");
+        c = new Converter(list,"path", "prenom", "bob", null);
         listQuerries = c.ConvertToPrologQuery();
         assertTrue(listQuerries.isEmpty());
 
@@ -59,9 +59,9 @@ public class testConverter {
         list.add("Lawfullness");
         list.add("Right-to-access");
         list.add("je ne suis pas un principe compatible");
-        c = new Converter(list,"path", null, null);
+        c = new Converter(list,"path", null, null, null);
         listQuerries = c.ConvertToPrologQuery();
-        String queryExpected = "?− legal(P, D, C, T G, T).";
+        String queryExpected = "?− legal(P, D, C, TG, T).";
         String queryExpected2 = "?− rigthAccess(S).";
 
         int numberExpected = 2;
